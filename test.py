31ff8a5abd8cb3479ff1518f5e423feeb8e5beb2
Nathanserra <nathanserra99@gmail.com>
2023-05-22 20:42:27 -0300

    Triangulação Função de completude do identifier

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from models import Author
 
-author1 = Author('Brasileiro', 'Brasil', 'São Paulo', 'SP', '123456789', '987654321')
+author1 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '123456789', '987654321')
+author2 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '', '987654321')
+author3 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '', '')
+author4 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '123456789', '')
 
 
 def test_indetifier_completeness():
     assert author1.identifier.completeness() == 0 
+    assert author2.identifier.completeness() == 1
+    assert author3.identifier.completeness() == 0
+    assert author4.identifier.completeness() == 1
