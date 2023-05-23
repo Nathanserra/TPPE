076b64db19869e6f9661f0d36c7785a841c44c4f
Nathanserra <nathanserra99@gmail.com>
2023-05-22 21:02:35 -0300

    Falsificação função de completude do article

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
-from models import Author
+from models import Author, Article
 
 author1 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '123456789', '987654321')
 author2 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '', '987654321')
 author3 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '', '')
 author4 = Author('Brasil', 'Brasil', 'São Paulo', 'SP', '123456789', '')
-
 author5 = Author('', '', '', 'SP', '123456789', '987654321')
 author6 = Author('Brasil', '', '', 'SP', '', '987654321')
 
+article1 = Article('Teste', '15/05/2023', 'Portugues')
+
+article1.addAuthor(author1)
+article1.addAuthor(author2)
+article1.addAuthor(author3)
 
 def test_indetifier_completeness():
     assert author1.identifier.completeness() == 0 
@@ -22,3 +26,6 @@ def test_author_completeness():
     assert author4.completeness() == 1
     assert author5.completeness() == 0.125
     assert author6.completeness() == 0.75
+
+def test_article_completeness():
+    assert article1.completeness() == 0.875
